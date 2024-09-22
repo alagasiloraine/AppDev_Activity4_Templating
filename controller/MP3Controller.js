80b19f50ae4993075299aa191ac436a84c4c3ccf
Loraine Alagasi <loraineborjaalagasi@gmail.com>
2024-09-22 19:10:16 +0700

    initial commit

--- controller/MP3Controller.js
+++ controller/MP3Controller.js
@@ -1,19 +1,25 @@
 const mp3Controller = {
-  index: (req, res) => {
-      res.render('home');
-  },
-  playlist: (req, res) => {
-      res.render('playlist');
-  },
-  artist: (req, res) => {
-      res.render('artist');
-  },
-  albums: (req, res) => {
-      res.render('albums');
-  },
-  favorites: (req, res) => {
-      res.render('favorites');
-  }
+    index: (req, res) => {
+        res.render('home', {title: 'MP3'});
+    },
+    albums: (req, res) => {
+        res.render('albums', {title: 'MP3'});
+    },
+    event: (req, res) => {
+        res.render('event', {title: 'MP3'});
+    },
+    news: (req, res) => {
+        res.render('news', {title: 'MP3'});
+    },
+    contact: (req, res) => {
+        res.render('contact', {title: 'MP3'});
+    },
+    login: (req, res) => {
+        res.render('login', {title: 'MP3'});
+    },
+    elements: (req, res) => {
+        res.render('elements', {title: 'MP3'});
+    }
 };
 
 module.exports = mp3Controller;
--- routes/router.js
+++ routes/router.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const mp3Controller = require("../controller/MP3Controller");
 
 router.get('/', mp3Controller.index);
-router.get('/mp3/playlist', mp3Controller.playlist);
-router.get('/mp3/artist', mp3Controller.artist);
 router.get('/mp3/albums', mp3Controller.albums);
-router.get('/mp3/favorites', mp3Controller.favorites);
+router.get('/mp3/event', mp3Controller.event);
+router.get('/mp3/news', mp3Controller.news);
+router.get('/mp3/contact', mp3Controller.contact);
+router.get('/mp3/login', mp3Controller.login);
+router.get('/mp3/elements', mp3Controller.elements);
 
 module.exports = router;
